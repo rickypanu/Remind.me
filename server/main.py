5502441502cfaeb7aebc46c7e3412349b2c560aa
rickypanu <rickypanu2005@gmail.com>
2026-06-26 11:53:30 +0530

    origins upd

--- server/main.py
+++ server/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="RemindMe API")
 origins = [
     "https://remindme-psi.vercel.app", 
     "http://localhost:3000",           
-    "http://localhost:5173",           
+    "http://localhost:5173",   ]        
 
 app.add_middleware(
     CORSMiddleware,
