ae2259502642745bf04586dbd9be80186f5be0d5
rickypanu <rickypanu2005@gmail.com>
2026-06-27 10:45:16 +0530

    feat: configure application as installable PWA

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      manifest: false // Tells the plugin to use your existing public/manifest.json
+    })
   ],
 })
